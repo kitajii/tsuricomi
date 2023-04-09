f3f4c2c0498457ca0bbd49da4d85565edb9afffe
kitajii <kazu.rmcf@gmail.com>
2023-04-09 13:32:15 +0900

    react-iconsインストール、サイトメインアイコンを変更

--- src/resources/js/Layouts/AdminAuthenticated.jsx
+++ src/resources/js/Layouts/AdminAuthenticated.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import ApplicationLogo from '@/Components/ApplicationLogo';
+import { GiBoatFishing } from 'react-icons/gi';
 import Dropdown from '@/Components/Dropdown';
 import NavLink from '@/Components/NavLink';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
@@ -15,7 +15,7 @@ export default function Authenticated({ auth, header, children }) {
                         <div className="flex">
                             <div className="shrink-0 flex items-center">
                                 <Link href="/">
-                                    <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800" />
+                                    <GiBoatFishing size={40} />
                                 </Link>
                             </div>
 
